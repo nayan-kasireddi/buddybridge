a0a816f70b7b60739dc245322661996a1821f90b
Nayan Kasireddi <nayank1508@Nayans-MacBook-Pro.local>
2025-06-04 15:33:45 -0500

    ui for frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { MantineProvider } from '@mantine/core';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider>
       <App />
     </MantineProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
